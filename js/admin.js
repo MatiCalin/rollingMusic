19b9d0ca1735bed003be96b7087f238d970a1d30
Matias Calin <maticalin@gmail.com>
2022-11-22 19:58:21 -0300

    Pagina estilos. Botones de lista funcionando

--- js/admin.js
+++ js/admin.js
@@ -6,16 +6,17 @@ const user = JSON.parse(localStorage.getItem("usuario"));
 class Cancion {
     constructor(codigo, titulo, interprete, imagen) {
         this.codigo = codigo;
+        this.imagen = imagen;
         this.titulo = titulo;
         this.interprete = interprete;
-        this.imagen = imagen;
+        
     }
 }
 
 // if(user.id === 1234){
 //     const tablaUsuarios = document.querySelector("#tableUser");
     const tablaCanciones = document.querySelector("#bodyTable");
-    const myModal = document.querySelector("#myModal");
+    const myModal = document.querySelector("#changingModal");
     const content = document.querySelector("#bodyModificaModal");
     //const usuarios = JSON.parse(localStorage.getItem("usuarios")); //usuarios es la key por lo que en el registro quien se encargue de armar el almacenamiento en el local storage debe guardar los usuarios con esta key
 
@@ -41,13 +42,15 @@ class Cancion {
     function cargarTablaCanciones(){
         canciones.forEach((songs) => {
             let tr = document.createElement("tr");
-
             tr.innerHTML = `
                 <th scope="row">${songs.imagen}</th>
                 <td>${songs.titulo}</td>
                 <td>${songs.interprete}</td>
-                <a href="#" class="btn btn-warning" onclick="modificarSongs(${songs.codigo})"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> 
-            <a href="#" class="btn btn-danger" onclick="borrarSongs(${songs.codigo})"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                <div>
+                <a href="#" class="btn btn-warning" onclick="modificarSongs(${songs.codigo})"><i class="fa-solid fa-user-pen"></i></a> 
+            <a href="#" class="btn btn-danger" onclick="borrarSongs(${songs.codigo})"><i class="fa-solid fa-trash-can"></i></a>
+                </div>
+                
             `;
 
             tablaCanciones.appendChild(tr);
@@ -61,11 +64,11 @@ class Cancion {
         if (songs === null) {
             nuevo = true;
             content.innerHTML = `
-            <div class="modal-body" id="bodyModificaModal">
-                            <div class="form-group">
+            <div class="form-group">
                                 <label for="titulo">Titulo de canción</label>
                                 <input type="text" class="form-control" id="titulo" value="" autocomplete="off">
                             </div>
+                            
                             <div class="form-group">
                                 <label for="interprete">Nombre de artista</label>
                                 <input type="text" class="form-control" id="interprete" value="" autocomplete="off">
@@ -74,15 +77,15 @@ class Cancion {
                                 <label for="discImg">Imagen de disco</label>
                                 <input type="text" class="form-control" id="discImg" value="" autocomplete="off">
                             </div>
-                        </div>
+                            
                         `;
         } else {
             content.innerHTML = `
-            <div class="modal-body" id="bodyModificaModal">
-                            <div class="form-group">
+            <div class="form-group">
                                 <label for="titulo">Titulo de canción</label>
                                 <input type="text" class="form-control" id="titulo" value="${songs.titulo}" autocomplete="off">
                             </div>
+                            
                             <div class="form-group">
                                 <label for="interprete">Nombre de artista</label>
                                 <input type="text" class="form-control" id="interprete" value="${songs.interprete}" autocomplete="off">
@@ -91,30 +94,46 @@ class Cancion {
                                 <label for="discImg">Imagen de disco</label>
                                 <input type="text" class="form-control" id="discImg" value="${songs.imagen}" autocomplete="off">
                             </div>
+                            
                         </div>
                         `;
         }
     }
     
+
     //hago que se carguen todas las canciones
     function cargarCanciones(e) {
+        tablaCanciones.innerHTML = "";
         e.preventDefault();
 
         //me traigo todos los datos   
         const codigo = new Date().getTime(); 
-        const imagen = document.querySelector("#discImg").value;
+        let imagen = document.querySelector("#discImg").value;
         const titulo = document.querySelector("#titulo").value;
         const interprete = document.querySelector("#interprete").value;
 
-        // if (imagen === "") {
-        //     imagen = "https://www.pindula.co.zw/images/a/a7/No_Image.jpg";
-        // }
+        if (imagen === "") {
+            imagen = "https://www.pindula.co.zw/images/a/a7/No_Image.jpg";
+        }
 
-        const newSong = new Cancion(codigo, titulo, interprete, imagen);
-        canciones = [...canciones, newSong];
+        if (nuevo) {
+            const newSong = new Cancion(codigo, titulo, interprete, imagen);
+            canciones = [...canciones, newSong];
+        }else {
+            let index = canciones.findIndex((songs) =>{
+                return songs.codigo === cancion.codigo;
+            });
+
+            canciones[index].imagen = imagen;
+            canciones[index].titulo = titulo;
+            canciones[index].interprete = interprete;
+        }
+
         localStorage.setItem("canciones", JSON.stringify(canciones));
         
         $(myModal).modal("hide");
+
+        cargarTablaCanciones();
     }
 
     function borrarSongs(codigo) {
@@ -122,6 +141,17 @@ class Cancion {
         canciones = canciones.filter( (cancion) => cancion.codigo !== codigo);
         localStorage.setItem("canciones", JSON.stringify(canciones));
         cargarTablaCanciones(); 
+    }
+
+    function modificarSongs(codigo) {
+        nuevo = false;
+        cancion = canciones.find( (music) => {
+            return music.codigo === codigo
+        });
+
+        abrirModal(cancion);
+
+        $(myModal).modal("show");
     }
 // }else{
 //     location.href = "home.html"
