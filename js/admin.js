f438cd0f017ad270cf42437f59b0981626662580
Matias Calin <maticalin@gmail.com>
2022-12-01 19:15:27 -0300

    Pagina administración finalizada y hojas de estilo

--- js/admin.js
+++ js/admin.js
@@ -43,13 +43,18 @@ class Cancion {
         canciones.forEach((songs) => {
             let tr = document.createElement("tr");
             tr.innerHTML = `
-                <th scope="row" class="text-center"><img src="${songs.imagen}" alt="${songs.titulo}"></th>
-                <td class="text-center songData">${songs.titulo}</td>
-                <td class="text-center songData">${songs.interprete}</td>
-                <div>
-                <a href="#" class="btn btn-warning my-1" onclick="modificarSongs(${songs.codigo})"><i class="fa-solid fa-user-pen"></i></a> 
-            <a href="#" class="btn btn-danger my-1 px-3" onclick="borrarSongs(${songs.codigo})"><i class="fa-solid fa-trash-can"></i></a>
-                </div>
+                <th scope="row" class="text-center"><img src="${songs.imagen}" alt="${songs.titulo}" class="rounded my-1 tableImg"></th>
+                <td class="text-center text-capitalize songData">${songs.titulo}</td>
+                <td class="text-center text-uppercase songData">${songs.interprete}</td>
+                
+                    <div class="collapseBtn">
+                        <a href="#" class="btn my-1" onclick="modificarSongs(${songs.codigo})"><i class="fa-solid fa-user-pen"></i></a> 
+                        <a href="#" class="btn my-1 px-3" onclick="borrarSongs(${songs.codigo})"><i class="fa-solid fa-trash-can"></i></a>
+                        <button>
+                            >
+                        </button>
+                    </div>
+                
                 
             `;
 
@@ -65,34 +70,34 @@ class Cancion {
             nuevo = true;
             content.innerHTML = `
             <div class="form-group">
-                                <label for="titulo">Titulo de canción</label>
-                                <input type="text" class="form-control my-2" id="titulo" value="" autocomplete="off">
+                                <label for="titulo" class="fw-normal fs-5">Titulo de canción</label>
+                                <input maxlength="25" type="text" class="form-control my-2 fs-4" id="titulo" value="" autocomplete="off" required>
                             </div>
                             
                             <div class="form-group">
-                                <label for="interprete">Nombre de artista</label>
-                                <input type="text" class="form-control my-2" id="interprete" value="" autocomplete="off">
+                                <label for="interprete" class="fw-normal fs-5">Nombre de artista</label>
+                                <input maxlength="25" type="text" class="form-control my-2 fs-4" id="interprete" value="" autocomplete="off" required>
                             </div>
                             <div class="form-group">
-                                <label for="discImg">Imagen de disco</label>
-                                <input type="text" class="form-control my-2" id="discImg" value="" autocomplete="off">
+                                <label for="discImg" class="fw-normal fs-5">Imagen de disco</label>
+                                <input type="url" class="form-control my-2 fs-4" id="discImg" value="" autocomplete="off">
                             </div>
                             
                         `;
         } else {
             content.innerHTML = `
             <div class="form-group">
-                                <label for="titulo">Titulo de canción</label>
-                                <input type="text" class="form-control" id="titulo" value="${songs.titulo}" autocomplete="off">
+                                <label for="titulo" class="fw-normal fs-5">Titulo de canción</label>
+                                <input maxlength="25" type="text" class="form-control my-2 fs-4" id="titulo" value="${songs.titulo}" autocomplete="off" required>
                             </div>
                             
                             <div class="form-group">
-                                <label for="interprete">Nombre de artista</label>
-                                <input type="text" class="form-control" id="interprete" value="${songs.interprete}" autocomplete="off">
+                                <label for="interprete" class="fw-normal fs-5">Nombre de artista</label>
+                                <input maxlength="25" type="text" class="form-control my-2 fs-4" id="interprete" value="${songs.interprete}" autocomplete="off">
                             </div>
                             <div class="form-group">
-                                <label for="discImg">Imagen de disco</label>
-                                <input type="text" class="form-control" id="discImg" value="${songs.imagen}" autocomplete="off">
+                                <label for="discImg" class="fw-normal fs-5">Imagen de disco</label>
+                                <input type="url" class="form-control my-2 fs-4" id="discImg" value="${songs.imagen}" autocomplete="off">
                             </div>
                             
                         </div>
@@ -100,7 +105,7 @@ class Cancion {
         }
     }
     
-
+    
     //hago que se carguen todas las canciones
     function cargarCanciones(e) {
         tablaCanciones.innerHTML = "";
