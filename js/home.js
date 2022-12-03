07ae86a4769031e95e87a27eeda69b3a2df1777b
Miguel Montoro <miguelmontoro1997@gmail.com>
2022-12-03 20:01:08 -0300

    se avanza con js

--- js/home.js
+++ js/home.js
@@ -1,45 +1,45 @@
-// si un usuario no se encuentra logeado
+//si un usuario no se encuentra logeado
 
-// let user = JSON.parse(localStorage.getItem("...")) 
+let user = JSON.parse(localStorage.getItem("usuario")) 
 
-// if(user.email === undefined) {
-//     location.href = "login.html"
-// }
+if(user.email === undefined) {
+    location.href = "login.html"
+}
 
-// traigo el arreglo de canciones.
+//traigo el arreglo de canciones.
 
-// let ... = JSON.parse(localStorage.getItem("...")) || [];
+let ... = JSON.parse(localStorage.getItem("...")) || [];
 
-// //variable donde voy a poner las cards
+//variable donde voy a poner las cards
 
 
-//  let contenedor = document.querySelcetor("#...");
+let contenedor = document.querySelcetor("#...");
 
-// function cargarCard() {
-//     .forEach( (...)=> {
-//     div.classList = "col col-md-6 col-lg mb-3";
+function cargarCard() {
+    .forEach( (...)=> {
+    div.classList = "col col-md-6 col-lg mb-3";
     
 
 
-//     div.innerHTML =`
-//     <div class="card">
-//                 <div class="cover-card">
-//                     <img src="https://i1.sndcdn.com/artworks-000024521404-ggqxj7-t500x500.jpg" alt="imagen">
-//                 </div>
-//                 <h2>Que es Dios?</h2>
-//                 <p class="interprete">Las pastillas del abuelo</p>
-//                 <small class=" text-ligth text-muted">5:49 min</small>
-//                 <hr>
-//                 <div class="footer-card">
-//                     <a href="#" class="u-full-width button-primary button input agregar-lista"data-id="cancion2">Agregar a mi lista +</a>
-//                     <a href="#"><i>Resproducir</i></a>
-//                 </div>
-//             </div>
-//     `;
+    div.innerHTML =`
+    <div class="card">
+                <div class="cover-card">
+                    <img src="${.imagen}" alt="imagen">
+                </div>
+                <h2>${.titulo}</h2>
+                <p class="interprete">${.interprete}</p>
+                <small class=" text-ligth text-muted">5:49 min</small>
+                <hr>
+                <div class="footer-card">
+                    <a href="#" class="u-full-width button-primary button input agregar-lista"data-id="cancion2">Agregar a mi lista +</a>
+                    <a href="#"><i>Resproducir</i></a>
+                </div>
+            </div>
+    `;
 
-//     contenedor.appendChild(div);
+    contenedor.appendChild(div);
 
-//     });
-// }
+    });
+}
 
-// cargarCard();
+cargarCard();
