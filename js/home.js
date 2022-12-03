591b92c3cd39d2ee3a10d59cc96829fbd21938a0
Miguel Montoro <miguelmontoro1997@gmail.com>
2022-12-03 19:31:21 -0300

    logica js incompleta

--- js/home.js
+++ js/home.js
@@ -1,1 +1,45 @@
-alert("hola mundo")
+// si un usuario no se encuentra logeado
+
+// let user = JSON.parse(localStorage.getItem("...")) 
+
+// if(user.email === undefined) {
+//     location.href = "login.html"
+// }
+
+// traigo el arreglo de canciones.
+
+// let ... = JSON.parse(localStorage.getItem("...")) || [];
+
+// //variable donde voy a poner las cards
+
+
+//  let contenedor = document.querySelcetor("#...");
+
+// function cargarCard() {
+//     .forEach( (...)=> {
+//     div.classList = "col col-md-6 col-lg mb-3";
+    
+
+
+//     div.innerHTML =`
+//     <div class="card">
+//                 <div class="cover-card">
+//                     <img src="https://i1.sndcdn.com/artworks-000024521404-ggqxj7-t500x500.jpg" alt="imagen">
+//                 </div>
+//                 <h2>Que es Dios?</h2>
+//                 <p class="interprete">Las pastillas del abuelo</p>
+//                 <small class=" text-ligth text-muted">5:49 min</small>
+//                 <hr>
+//                 <div class="footer-card">
+//                     <a href="#" class="u-full-width button-primary button input agregar-lista"data-id="cancion2">Agregar a mi lista +</a>
+//                     <a href="#"><i>Resproducir</i></a>
+//                 </div>
+//             </div>
+//     `;
+
+//     contenedor.appendChild(div);
+
+//     });
+// }
+
+// cargarCard();
